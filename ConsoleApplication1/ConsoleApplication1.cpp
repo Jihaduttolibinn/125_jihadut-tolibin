67d293e379f88286954d56368e8c5f094c8587ad
jihad <jihaduttolibin02@gmail.com>
2024-02-26 15:18:45 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ //begin
+	// numeric nJejeri, nLuas
+	// display ' Masukin jejari= '
+	// accept mjejari
+	// compute nLuas = 3.14* r * r
+	// display 'Luasnya= '+ nLuas
+ //end
+	double nJejari, nLuas;
+	cout << "Masukan Jejari = ";
+	cin >> nJejari;
+	nLuas = 3.14 * nJejari * nJejari;
+	cout << "Luasnya = " << nLuas << endl;
+	system("pause");
+
+}
+
